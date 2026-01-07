062f3bf1512a0088d53959cab122a82718de1fb6
עינת גן אל <ganel.einat@gmail.com>
2026-01-07 16:02:24 +0000

    החזרת צ'אט alice-and-bot

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,7 +6,6 @@ import Footer from './components/navigation/Footer';
 import { Loader2 } from 'lucide-react';
 import LogoCarousel from './components/home/LogoCarousel';
 import CookieConsent from './components/shared/CookieConsent';
-import ChatButton from './components/shared/ChatButton';
 import { migrateSettings } from "@/functions/migrateSettings";
 
 function StyleInjector() {
@@ -147,7 +146,6 @@ function AppContent({ children, currentPageName }) {
             </main>
             {currentPageName !== 'Home' && <LogoCarousel />}
             <Footer />
-            <ChatButton />
             <CookieConsent />
         </div>
     );
